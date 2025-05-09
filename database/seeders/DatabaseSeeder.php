d48d38409c63d2d2fc5b75b9bba3bbe3a721375f
JushuaTaduran <taduranjushua2@gmail.com>
2025-05-09 08:24:04 +0800

    Modified DockerFile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
-
+    {
         if (app()->environment(['local', 'staging'])) {
             $this->call([
                 UsersTableSeeder::class,
